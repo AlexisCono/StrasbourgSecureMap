b23a3becbbb5100cfd0cf36226bc5e6acb13bdba
Mathis Bouhanna <bouhanna.mathis06@gmail.com>
2024-03-14 17:10:25 +0100

    amélioration graphique, nouveau style

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -9,12 +9,13 @@ const HomePage = () => {
     <div>
       <Banner />
       <header className="App-header">
-      <img src="./image/logoSSM.png" alt="HomePage_logoSSM"/>        
-      <div className="buttonAccueil">
+      <img src="./image/logoSSM.png" alt="HomePage_logoSSM" style={{ width: '25%', height: 'auto', marginTop: '-100px'}} />        
+      <div className="buttonAccueil" style={{ marginTop: '20px'}}>
         <Button href="/interactive_map" classe="buttonHref">
           Nouveau projet
         </Button>
-        <Button href="/backups" classe="buttonHref">Projets sauvegardés</Button>
+        <div style={{ width: '50px' }}></div> {/* Espace entre les boutons */}
+        <Button href="/backups" classe="buttonHref"  >Projets sauvegardés</Button>
       </div>
       </header>
     </div>
--- src/InteractiveMap.jsx
+++ src/InteractiveMap.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import Map from "./components/map";
 import Banner from "./Banner";
 import Button from "./Button";
-import TimePicker from "./components/TimePicker";
 import "./styles/Button.css";
 
 function InteractiveMap() {
   return (
     <div>
       <Banner>
-        <Button href="/" classe="buttonRetourAcc">
-          Accueil
+        <Button href="/" classe="buttonRetourAcc" >
+          MENU
         </Button>
       </Banner>
-      <TimePicker/>
       <Map />
     </div>
   );

b2e2aaa752d17cbb87c4c3019aab199d9d6c722f
Nathan Louvet <louvet.nathan@icloud.com>
2024-04-10 17:10:09 +0200

    modification bug Mathis

--- src/MapboxApiKeyForm.jsx
+++ src/MapboxApiKeyForm.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const MapboxApiKeyForm = () => {
-  const [newApiKey, setNewApiKey] = useState('');
-  const [mapboxApiKey, setMapboxApiKey] = useState('');
+  const [newApiKey, setNewApiKey] = useState("");
+  const [mapboxApiKey, setMapboxApiKey] = useState("");
 
   // Au chargement du composant, récupérer la valeur de mapboxApiKey depuis localStorage
   useEffect(() => {
-    const storedApiKey = localStorage.getItem('mapboxApiKey');
+    const storedApiKey = localStorage.getItem("mapboxApiKey");
     if (storedApiKey) {
       setMapboxApiKey(storedApiKey);
     }
@@ -19,30 +19,43 @@ const MapboxApiKeyForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Mettre à jour la clé Mapbox avec la nouvelle valeur dans localStorage
-    localStorage.setItem('mapboxApiKey', newApiKey);
+    localStorage.setItem("mapboxApiKey", newApiKey);
     setMapboxApiKey(newApiKey);
-    console.log('Nouvelle clé Mapbox :', newApiKey);
+    console.log("Nouvelle clé Mapbox :", newApiKey);
   };
 
   return (
-    
-      <form class="form" onSubmit={handleSubmit}>
-      <div class="title">Pour utiliser Strasbourg Secure Map,<br/> <span>une clé "MapBox" est nécessaire</span></div>
-        <label htmlFor="newApiKey"></label>
-        <input
-         class="input" name="text"  type="text"
-          id="newApiKey"
-          placeholder="Veuillez entrer une nouvelle clé MapBox..."  
-          value={newApiKey}
-          style={{color:'#00114d'}}
-          onChange={handleInputChange}
-        />
-            
-       <button class="button-confirm" type="submit">Enregistrer</button>
-        <div style={{color: '#FFFF', fontSize:'80%', fontFamily: 'Consolas,monaco,monospace'}}> <p>Clé actuelle : {mapboxApiKey}</p>
-        </div>
-        </form>
-      
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="title">
+        Pour utiliser Strasbourg Secure Map,
+        <br /> <span>une clé &quot;MapBox&quot; est nécessaire</span>
+      </div>
+      <label htmlFor="newApiKey"></label>
+      <input
+        className="input"
+        name="text"
+        type="text"
+        id="newApiKey"
+        placeholder="Veuillez entrer une nouvelle clé MapBox..."
+        value={newApiKey}
+        style={{ color: "#00114d" }}
+        onChange={handleInputChange}
+      />
+
+      <button className="button-confirm" type="submit">
+        Enregistrer
+      </button>
+      <div
+        style={{
+          color: "#FFFF",
+          fontSize: "80%",
+          fontFamily: "Consolas,monaco,monospace",
+        }}
+      >
+        {" "}
+        <p>Clé actuelle : {mapboxApiKey}</p>
+      </div>
+    </form>
   );
 };
 

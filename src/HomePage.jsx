c4431952b5c2a372dac3dbcdb87ba5018a021641
Nathan Louvet <louvet.nathan@icloud.com>
2024-04-11 00:56:50 +0200

    j'essaye de passer les donnée JSON avec le href au composant InteractiveMap

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
-import Button from "./Button"; // Assurez-vous d'importer correctement le composant Button
+import Button from "./Button";
 import Banner from "./Banner";
-import "./styles/Button.css";
 import MapboxApiKeyForm from "./MapboxApiKeyForm";
+import InteractiveMap from "./InteractiveMap"; // Importez votre composant InteractiveMap
 
 const HomePage = () => {
+  const [jsonData, setJsonData] = useState(null);
+
+  const handleFileChange = async (event) => {
+    const selectedFile = event.target.files[0];
+    if (!selectedFile) return;
+
+    try {
+      const fileContent = await selectedFile.text();
+      const parsedData = JSON.parse(fileContent);
+      setJsonData(parsedData);
+    } catch (error) {
+      console.error("Error parsing JSON file:", error);
+    }
+  };
 
   return (
     <div>
       <Banner />
       <header className="App-header">
-        <img src="./image/Logo4.png"  style={{width: '18%', height: 'auto', marginTop: '1%'}}></img>       
-        <div className="buttonAccueil" style={{ marginTop: '1%'}}>
-          <Button href="/interactive_map" classe="buttonHref">
+        <img
+          src="./image/Logo4.png"
+          style={{ width: "18%", height: "auto", marginTop: "1%" }}
+          alt="Logo"
+        ></img>
+        <div className="buttonAccueil" style={{ marginTop: "1%" }}>
+          <Button href="/interactiveMap" classe="buttonHref">
             Nouveau projet
           </Button>
-          <div style={{ width: '50px' }}></div> {/* Espace entre les boutons */}
-          <Button href="/backups" classe="buttonHref"  >Projets sauvegardés</Button>
+          <div style={{ width: "50px" }}></div>
         </div>
-        <MapboxApiKeyForm />
+        <input type="file" onChange={handleFileChange} />
       </header>
-      
     </div>
   );
 };
 
 export default HomePage;
-

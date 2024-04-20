4978af11318ea71f77de7ba76e52313ed1b232ab
Nathan Louvet <louvet.nathan@icloud.com>
2024-04-20 19:07:01 +0200

    fixation bug nom fichier

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/App.css";
 import PropTypes from "prop-types";
 import Button from "./Button";
-import "../styles/button.css";
+import "../styles/buttonStyle.css";
 
 const Banner = ({ children }) => {
   return (
